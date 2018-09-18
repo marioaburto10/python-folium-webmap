9e163f4210915982bb8d7f6474772c861d14ead9
Mario Aburto <marioaburto10@gmail.com>
2018-09-18 17:15:22 -0400

    bring in volcano data and iterate through it using pandas

--- map1.py
+++ map1.py
@@ -1,13 +1,30 @@
 # importing folium dependency that will be used to
 import folium
+# importing pandas to do some data processing
+import pandas
 
-# using the Map method to create a web map at the specified location, in this case Wshington DC. Have the map render already zoomed in with a specific tile style
-map = folium.Map(location=[38.926702, -77.018698], zoom_start=6, tiles="Mapbox Bright") 
+# using pandas to read and bring in our text file with all our volcano data
+data = pandas.read_csv("Resources/volcano_data.txt")
 
-# create a pop up marker at a certain location with a message and add it to the map
+# extract certain columns from the data file and turn them into lists so that we can interate through them
+lat = list(data["LAT"])
+lng = list(data["LON"])
+elev = list(data["ELEV"])
+
+# using the Map method to create a web map at the specified location, in this case Sacramento, CA. Have the map render already zoomed in with a specific tile style
+map = folium.Map(location=[38.554587, -121.511129], zoom_start=6, tiles="Mapbox Bright") 
+
+# create a feature froup using folium to create our markers on the map
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.91, -77.2], popup="Hi I am a Marker", icon=folium.Icon(color="green")))
+
+# use a for loop to iterate through every latitude, longitude, and elevation
+# use the key word zip to be able to pass in three lists and be able to iterate through all three at the same time
+for lt, lg, el in zip(lat, lng, elev):
+	# create a pop up marker at a certain location with a message of each volcano elevation and add it to the map
+	fg.add_child(folium.Marker(location=[lt, lg], popup=str(el)+" meters", icon=folium.Icon(color="green")))
+
+# add each dynamically created feature group (marker) to the map
 map.add_child(fg)
 
-# this will generate a web map and save it as an html file
-map.save("Map1.html");
+# generate a web map and save it as an html file
+map.save("Map1.html")
