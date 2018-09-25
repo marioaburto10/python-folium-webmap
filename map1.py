67e2147943c79f05e304d057bbda534541be9b22
Mario Aburto <marioaburto10@gmail.com>
2018-09-25 17:34:47 -0400

    changing polygon layer color based on population

--- map1.py
+++ map1.py
@@ -14,9 +14,6 @@ elev = list(data["ELEV"])
 # using the Map method to create a web map at the specified location, in this case Sacramento, CA. Have the map render already zoomed in with a specific tile style
 map = folium.Map(location=[38.554587, -121.511129], zoom_start=6, tiles="Mapbox Bright") 
 
-# create a feature froup using folium to create our markers on the map
-fg = folium.FeatureGroup(name="My Map")
-
 # function that will return a color depending on the numerical value of the elevation of each volcano
 def maker_color_picker(elevation):
 	if elevation < 1000:
@@ -26,6 +23,9 @@ def maker_color_picker(elevation):
 	else:
 		return 'red'
 
+# create a feature froup using folium to create our markers on the map
+fg = folium.FeatureGroup(name="My Map")
+
 # use a for loop to iterate through every latitude, longitude, and elevation
 # use the key word zip to be able to pass in three lists and be able to iterate through all three at the same time
 for lt, lg, el in zip(lat, lng, elev):
@@ -33,7 +33,9 @@ for lt, lg, el in zip(lat, lng, elev):
 	fg.add_child(folium.CircleMarker(location=[lt, lg], radius=6, popup=str(el)+" meters", fill=True, fill_color=maker_color_picker(el), color="grey", fill_opacity=0.7))
 
 # adding GeoJson Polygon layer to the map by reading the world.json file
-fg.add_child(folium.GeoJson(data=(open("Resources/world.json", "r", encoding="utf-8-sig").read())))
+# it will create a polygon layer and the polygon color will be based on population
+fg.add_child(folium.GeoJson(data=open("Resources/world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {"fillColor" : "green" if x["properties"]["POP2005"] < 10000000 else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red" }))
 
 # add each dynamically created feature group (marker) to the map
 map.add_child(fg)
